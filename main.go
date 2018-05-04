c331abcba10e3c489b34102838c170bc80fe3b69
NC-KOREA\mylee <mylee@ncsoft.com>
2018-05-04 09:56:32 +0900

    add do func

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// Copyright 2010 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
@@ -12,47 +8,35 @@ import (
 )
 
 type Data struct {
-	num int
+	num   int
+	exter int
 }
 
-type DataAction struct {
-	do   func(data *Data) Data
-	data *Data
-}
-
-func Process(in <-chan *DataAction, out chan<- Data) {
-	for d := range in {
-		result := d.do(d.data)
-		out <- result
-	}
-}
-
-type Event struct {
-	action int
+type ChannelMessage struct {
 	param  interface{}
-	do     func(*Container, interface{})
+	result chan interface{}
+	do     func(*Container, interface{}, chan<- interface{})
 }
 
 type Container struct {
 	dataM  map[int]*Data
-	eventC chan Event
+	reqC   chan ChannelMessage
 	closeC chan bool
 }
 
 func NewContainer() *Container {
 	c := &Container{}
 	c.dataM = make(map[int]*Data)
-	c.eventC = make(chan Event)
+	c.reqC = make(chan ChannelMessage)
 	c.closeC = make(chan bool)
 	return c
 }
 
-func (c *Container) Process() {
+func (c *Container) Go() {
 	for {
 		select {
-		case ev := <-c.eventC:
-			fmt.Println("event:", ev.action)
-			ev.do(c, ev.param)
+		case req := <-c.reqC:
+			req.do(c, req.param, req.result)
 
 		case close := <-c.closeC:
 			if close {
@@ -63,51 +47,99 @@ func (c *Container) Process() {
 	}
 }
 
+func (c *Container) Add(data *Data) {
+	c.reqC <- ChannelMessage{
+		param: data,
+		do: func(c *Container, param interface{}, result chan<- interface{}) {
+			d := param.(*Data)
+			c.dataM[d.num] = d
+		},
+	}
+}
+
+func (c *Container) Remove(num int) {
+	c.reqC <- ChannelMessage{
+		param: num,
+		do: func(c *Container, param interface{}, result chan<- interface{}) {
+			num := param.(int)
+			delete(c.dataM, num)
+		},
+	}
+}
+
+func (c *Container) Get(num int) (*Data, bool) {
+	result := make(chan interface{})
+
+	c.reqC <- ChannelMessage{
+		param:  num,
+		result: result,
+		do: func(c *Container, param interface{}, result chan<- interface{}) {
+			result <- c.dataM[param.(int)]
+		},
+	}
+
+	data := <-result
+	return data.(*Data), (data.(*Data) != nil)
+}
+
+func (c *Container) Do(num int, do func(*Data) error) error {
+	result := make(chan interface{})
+
+	c.reqC <- ChannelMessage{
+		param:  num,
+		result: result,
+		do: func(c *Container, param interface{}, result chan<- interface{}) {
+			var err error
+			if v, ok := c.dataM[param.(int)]; ok {
+				err = do(v)
+			}
+			result <- err
+		},
+	}
+
+	e := <-result
+	if e == nil {
+		return nil
+	} else {
+		return e.(error)
+	}
+}
+
 func main() {
-	runCount := 100
+
 	rand.Seed(100)
 	cont := NewContainer()
-	go cont.Process()
+	go cont.Go()
 
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for i := 0; i < runCount; i++ {
-			cont.eventC <- Event{action: 1,
-				param: i,
-				do: func(c *Container, param interface{}) {
-					c.dataM[rand.Intn(10)] = &Data{num: param.(int)}
-				},
-			}
-		}
+
+		cont.Add(&Data{num: 1})
+		cont.Add(&Data{num: 2})
+		cont.Remove(2)
+		cont.Add(&Data{num: 3})
 	}()
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < runCount; i++ {
-			cont.eventC <- Event{action: 2,
-				do: func(c *Container, param interface{}) {
-					delete(c.dataM, rand.Intn(10))
-				},
-			}
-		}
-	}()
+	time.Sleep(1)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		for i := 0; i < runCount; i++ {
-			cont.eventC <- Event{action: 3,
-				do: func(c *Container, param interface{}) {
-					if v, ok := c.dataM[rand.Intn(10)]; ok {
-						fmt.Println("get", v.num)
-					}
-				},
-			}
+		cont.Do(1, func(d *Data) error {
+			d.exter = 3333
+			return nil
+		})
+
+		data, ok := cont.Get(1)
+		if ok {
+			fmt.Println(data.num, data.exter)
+			data.num++
+		} else {
+			fmt.Println("no data")
 		}
 	}()
 
